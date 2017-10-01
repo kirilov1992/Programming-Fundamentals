3f9e2a0838cb4efcaada582f124533f9ec472394
Krasimir Kirilov <31036812+kirilov1992@users.noreply.github.com>
2017-10-01 18:25:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+            
+
+
+            double full = volume / 100.0;
+            double cal = energyContent * full;
+            double sugars = sugar * full;
+
+            Console.WriteLine("{0}ml {1}:",volume,name);
+            Console.WriteLine("{0}kcal, {1}g sugars",cal,sugars);
+        }
+    }
+}
