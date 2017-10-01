2ef8f18661fe1d63a633a38321364cade27cc1ca
Krasimir Kirilov <31036812+kirilov1992@users.noreply.github.com>
2017-10-01 18:29:14 +0300

    Add files via upload

---
+++ CakeIngredients.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake_Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            bool prepare = true;
+            while (prepare)
+            {
+               
+                string input = Console.ReadLine();
+                if (input != "Bake!")
+                {
+                    Console.WriteLine($"Adding ingredient {input}.");
+                    count++;
+                }
+                
+
+                if (input =="Bake!")
+                {
+                    Console.WriteLine($"Preparing cake with {count} ingredients.");
+                    prepare = false;
+                    break;
+                }
+            }
+        }
+    }
+}
